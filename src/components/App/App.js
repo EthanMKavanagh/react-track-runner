31103babb963833f133510c681a6388400a93413
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-17 20:17:47 -0500

    got statuc html set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    recentRun: '',
+    longestRun: '',
+    runs: []
+  }
+
+  onAddRun = (runParam) => {
+    console.log('New run is:', runParam);
+    this.setState({
+      recentRun: '',
+      runs: [
+        ...this.state.runs,
+        runParam
+      ]
+    });
+  }
+
+  onChangeRun = (event) => {
+    this.setState({
+      recentRun: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
         <header>
           <h1>React Track Runner</h1>
         </header>
+        <body>
+          <input type="number" placeholder="Miles Ran" onChange={this.onChangeRun} value={this.state.recentRun} />
+          <button onClick={() => this.onAddRun(this.state.recentRun)}>Add New Run</button>
+          <p>
+            Recent Run: {this.state.recentRun}
+          </p>
+          <p>
+            Longest Run: {this.state.longestRun}
+          </p>
+        </body>
       </div>
     );
   }
